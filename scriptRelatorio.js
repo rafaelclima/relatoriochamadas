ac61a5362f0fda8962a312ecd6e26b8f294b2f8f
rafaelclima <rafaelclima.ti@gmail.com>
2023-03-30 12:07:37 -0300

    updt html+js

--- scriptRelatorio.js
+++ scriptRelatorio.js
@@ -6,27 +6,50 @@ const divCards = document.getElementById('menu')
 const dataInicial = document.getElementById('start-date')
 const dataFinal = document.getElementById('end-date')
 const inputDestino = document.getElementById('input-ramal-destino')
-
+//Campos das chamadas Internas Efetuadas
 const intEfetuadaOcupado = document.getElementById('internas-efetuada-ocupado')
 const intEfetuadaAtendida = document.getElementById('internas-efetuada-atendida')
 const intEfetuadaNaoAtendida = document.getElementById('internas-efetuada-nao-atendida')
 const intEfetuadaFalha = document.getElementById('internas-efetuada-falha')
-
+const tempoIntEfetuada = document.getElementById('tempo-chamada-interna-efetuada')
+//Campos das chamadas Internas Recebidas
+const intRecebidaOcupado = document.getElementById('internas-recebidas-ocupado')
+const intRecebidaAtendida = document.getElementById('internas-recebidas-atendida')
+const intRecebidaNaoAtendida = document.getElementById('internas-recebidas-nao-atendida')
+const intRecebidaFalha = document.getElementById('internas-recebidas-falha')
+const tempoIntRecebida = document.getElementById('tempo-chamada-interna-recebida')
+//Campos das chamadas Externas Efetuadas
 const extEfetuadaOcupado = document.getElementById('externas-efetuada-ocupado')
 const extEfetuadaAtendida = document.getElementById('externas-efetuada-atendida')
 const extEfetuadaNaoAtendida = document.getElementById('externas-efetuada-nao-atendida')
 const extEfetuadaFalha = document.getElementById('externas-efetuada-falha')
 const tempoExtEfetuada = document.getElementById('tempo-chamada-externa-efetuada')
-
+//Campos das chamadas Externas Recebidas
+const extRecebidaOcupado = document.getElementById('externas-recebida-ocupado')
+const extRecebidaAtendida = document.getElementById('externas-recebida-atendida')
+const extRecebidaNaoAtendida = document.getElementById('externas-recebida-nao-atendida')
+const extRecebidaFalha = document.getElementById('externas-recebida-falha')
+const tempoExtRecebida = document.getElementById('tempo-chamada-externa-recebida')
+//Campos do resumo das chamadas Efetuadas
 const resumoEfetuadaOcupado = document.getElementById('resumo-efetuada-ocupado')
 const resumoEfetuadaAtendida = document.getElementById('resumo-efetuada-atendida')
 const resumoEfetuadaNaoAtendida = document.getElementById('resumo-efetuada-nao-atendida')
 const resumoEfetuadaFalha = document.getElementById('resumo-efetuada-falha')
+//Campos do resumo das chamadas Recebidas
+const resumoRecebidaOcupado = document.getElementById('resumo-recebida-ocupado')
+const resumoRecebidaAtendida = document.getElementById('resumo-recebida-atendida')
+const resumoRecebidaNaoAtendida = document.getElementById('resumo-recebida-nao-atendida')
+const resumoRecebidaFalha = document.getElementById('resumo-recebida-falha')
 
 let qtdIntEfetuadaOcupado = 0
 let qtdIntEfetuadaAtendida = 0
 let qtdIntEfetuadaNaoAtendida = 0
 let qtdIntEfetuadaFalha = 0
+
+let qtdIntRecebidaOcupado = 0
+let qtdIntRecebidaAtendida = 0
+let qtdIntRecebidaNaoAtendida = 0
+let qtdIntRecebidaFalha = 0
 
 let qtdExtEfetuadaOcupado = 0
 let qtdExtEfetuadaAtendida = 0
@@ -34,19 +57,39 @@ let qtdExtEfetuadaNaoAtendida = 0
 let qtdExtEfetuadaFalha = 0
 let tempoMedioExtEfetuada = 0
 
+let qtdExtRecebidaOcupado = 0
+let qtdExtRecebidaAtendida = 0
+let qtdExtRecebidaNaoAtendida = 0
+let qtdExtRecebidaFalha = 0
+let tempoMedioExtRecebida = 0
+
 
 let arrCsv = []
 let duracaoMediaExt = 0
 let duracaoMediaInt = 0
+let duracaoMediaRecebidaInt = 0
+let duracaoMediaRecebidaExt = 0
 
-function segundosParaMinutos(segundos) {
+function segundosParaMinutosExtEfetuada(segundos) {
   const vlrTotal = segundos / qtdExtEfetuadaAtendida
   const minutos = Math.floor(vlrTotal / 60);
   const segundosRestantes = vlrTotal % 60;
   return `${minutos}m ${Math.round(segundosRestantes)}s`;
 }
-function segundosParaMinutosInt(segundos) {
-  const vlrTotal = segundos / qtdChamadasInt
+function segundosParaMinutosIntEfetuada(segundos) {
+  const vlrTotal = segundos / qtdIntEfetuadaAtendida
+  const minutos = Math.floor(vlrTotal / 60);
+  const segundosRestantes = vlrTotal % 60;
+  return `${minutos}m ${Math.round(segundosRestantes)}s`;
+}
+function segundosParaMinutosIntRecebida(segundos) {
+  const vlrTotal = segundos / qtdIntRecebidaAtendida
+  const minutos = Math.floor(vlrTotal / 60);
+  const segundosRestantes = vlrTotal % 60;
+  return `${minutos}m ${Math.round(segundosRestantes)}s`;
+}
+function segundosParaMinutosExtRecebida(segundos) {
+  const vlrTotal = segundos / qtdExtRecebidaAtendida
   const minutos = Math.floor(vlrTotal / 60);
   const segundosRestantes = vlrTotal % 60;
   return `${minutos}m ${Math.round(segundosRestantes)}s`;
@@ -94,6 +137,8 @@ filterBtn.addEventListener('click', function() {
           const mes = csvData.split("-")[1];
           let toNumber = Number(ramaisOrigem.Duracao)
           let toString = ramaisOrigem.Destino.toString()
+          let toNumberRecebida = Number(ramaisOrigem.Duracao)
+          const toStringRecebida = ramaisOrigem.Origem.toString()
           const regexFiltroData = /^(\d{4}-\d{2}-\d{2}).*/;
           const regexFiltroHora = /\d{4}-\d{2}-\d{2}\s(\d{2}:\d{2}:\d{2})/
           // const dataFiltrada = ramaisOrigem.Data.replace(regexFiltroData, "$1");
@@ -101,14 +146,13 @@ filterBtn.addEventListener('click', function() {
           const dataFim = new Date(dataFinal.value).getTime();
 
           if(dataInicial !== "" && dataFinal !== "") {
-            
-          //CHAMADAS REALIZADAS (Ramal = Origem)  
-          if(inputRamal.value == ramaisOrigem.Origem) {
-
             const dataFiltrada = ramaisOrigem.Data.replace(regexFiltroData, "$1");
             const horaFiltrada = ramaisOrigem.Data.match(regexFiltroHora)[1]
             // Filtra os dados dentro do intervalo de tempo
             const data = new Date(dataFiltrada).getTime();
+            
+          //CHAMADAS REALIZADAS (Ramal = Origem)  
+          if(inputRamal.value == ramaisOrigem.Origem) {
 
             if(data >= dataInicio && data <= dataFim) {
 
@@ -118,6 +162,8 @@ filterBtn.addEventListener('click', function() {
                 intEfetuadaOcupado.innerText = qtdIntEfetuadaOcupado                  
               }else if (toString.length <= 5 && ramaisOrigem.Estado === "ANSWERED" && toNumber > 1) {
                 qtdIntEfetuadaAtendida++
+                duracaoMediaInt+=toNumber
+                tempoIntEfetuada.innerText = segundosParaMinutosIntEfetuada(duracaoMediaInt) 
                 intEfetuadaAtendida.innerText = qtdIntEfetuadaAtendida                                    
               }else if (toString.length <= 5 && ramaisOrigem.Estado === "NO ANSWER") {
                 qtdIntEfetuadaNaoAtendida++
@@ -133,8 +179,8 @@ filterBtn.addEventListener('click', function() {
               }else if (toString.length > 5 && ramaisOrigem.Estado === "ANSWERED" && toNumber > 1) {
                 qtdExtEfetuadaAtendida++
                 extEfetuadaAtendida.innerText = qtdExtEfetuadaAtendida
-                tempoMedioExtEfetuada = (duracaoMediaExt + toNumber)
-                tempoExtEfetuada.innerText = segundosParaMinutos(tempoMedioExtEfetuada)                  
+                duracaoMediaExt+=toNumber
+                tempoExtEfetuada.innerText = segundosParaMinutosExtEfetuada(duracaoMediaExt)                  
               }else if (toString.length > 5 && ramaisOrigem.Estado === "NO ANSWER") {
                 qtdExtEfetuadaNaoAtendida++
                 extEfetuadaNaoAtendida.innerText = qtdExtEfetuadaNaoAtendida                  
@@ -146,9 +192,7 @@ filterBtn.addEventListener('click', function() {
               resumoEfetuadaOcupado.innerText = (qtdIntEfetuadaOcupado + qtdExtEfetuadaOcupado)
               resumoEfetuadaAtendida.innerText = (qtdIntEfetuadaAtendida + qtdExtEfetuadaAtendida)
               resumoEfetuadaNaoAtendida.innerText = (qtdIntEfetuadaNaoAtendida + qtdExtEfetuadaNaoAtendida)
-              resumoEfetuadaFalha.innerText = (qtdIntEfetuadaFalha + qtdExtEfetuadaFalha)
-
-                
+              resumoEfetuadaFalha.innerText = (qtdIntEfetuadaFalha + qtdExtEfetuadaFalha)               
                 
             }
           }
@@ -156,9 +200,42 @@ filterBtn.addEventListener('click', function() {
           if(inputRamal.value == ramaisOrigem.Destino){
             if(data >= dataInicio && data <= dataFim) {
               //CHAMADAS INTERNAS RECEBIDAS
-              if(toString.length <= 5 && ramaisOrigem.Estado === "BUSY"){
-                
+              if(toStringRecebida.length <= 5 && ramaisOrigem.Estado === "BUSY"){
+                qtdIntRecebidaOcupado++
+                intRecebidaOcupado.innerText = qtdIntRecebidaOcupado                
+              }else if (toStringRecebida.length <= 5 && ramaisOrigem.Estado === "ANSWERED" && toNumberRecebida > 1){
+                qtdIntRecebidaAtendida++
+                duracaoMediaRecebidaInt+=toNumberRecebida
+                tempoIntRecebida.innerText = segundosParaMinutosIntRecebida(duracaoMediaRecebidaInt) 
+                intRecebidaAtendida.innerText = qtdIntRecebidaAtendida                
+              }else if (toStringRecebida.length <= 5 && ramaisOrigem.Estado === "NO ANSWER"){
+                qtdIntRecebidaNaoAtendida++
+                intRecebidaNaoAtendida.innerText = qtdIntRecebidaNaoAtendida
+              }else if (toStringRecebida.length <= 5 && ramaisOrigem.Estado === "FAILED"){
+                qtdIntRecebidaFalha++
+                intRecebidaFalha.innerText = qtdIntRecebidaFalha
+
+              //CHAMADAS EXTERNAS RECEBIDAS  
+              }else if (toStringRecebida.length > 5 && ramaisOrigem.Estado === "BUSY"){
+                qtdExtRecebidaOcupado++
+                extEfetuadaOcupado.innerText = qtdExtRecebidaOcupado 
+              }else if (toStringRecebida.length > 5 && ramaisOrigem.Estado === "ANSWERED" && toNumberRecebida > 1) {
+                qtdExtRecebidaAtendida++
+                extRecebidaAtendida.innerText = qtdExtRecebidaAtendida
+                duracaoMediaRecebidaExt+=toNumberRecebida
+                tempoExtRecebida.innerText = segundosParaMinutosExtRecebida(duracaoMediaRecebidaExt)                                 
+              }else if (toStringRecebida.length > 5 && ramaisOrigem.Estado === "NO ANSWER") {
+                qtdExtRecebidaNaoAtendida++
+                extRecebidaNaoAtendida.innerText = qtdExtRecebidaNaoAtendida                  
+              }else if (toStringRecebida.length > 5 && ramaisOrigem.Estado === "FAILED") {
+                qtdExtRecebidaFalha++
+                extRecebidaFalha.innerText = qtdExtRecebidaFalha                  
               }
+              //RESUMO DE CHAMADAS RECEBIDAS
+              resumoRecebidaOcupado.innerText = (qtdIntRecebidaOcupado + qtdExtRecebidaOcupado)
+              resumoRecebidaAtendida.innerText = (qtdIntRecebidaAtendida + qtdExtRecebidaAtendida)
+              resumoRecebidaNaoAtendida.innerText = (qtdIntRecebidaNaoAtendida + qtdExtRecebidaNaoAtendida)
+              resumoRecebidaFalha.innerText = (qtdIntRecebidaFalha + qtdExtRecebidaFalha)
                             
             }
           }
